e8e25b7717f08e26ddeaed31a2dcfa2817c76fde
Grizeu <58784325+iggdra@users.noreply.github.com>
2025-03-13 17:02:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -517,7 +517,7 @@ if __name__ == "__main__":
     template_bot = Q1TemplateBot(
         research_reports_per_question=1,
         predictions_per_research_report=5,
-        use_research_summary_to_forecast=True,
+        use_research_summary_to_forecast=False,
         publish_reports_to_metaculus=True,
         folder_to_save_reports_to=None,
         skip_previously_forecasted_questions=False,
